644fd6d6fe39f227e72cc58a24eb967640ee9504
David Eliseo <emartinez@MacBook-Pro-de-David.local>
2018-09-01 12:14:15 -0600

    Clorofila

---
+++ platforms/ios/www/js/getanalsem.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function()
+ {
+   var url="https://climapesca.org/api/get_category_posts/?id=6/";
+   //var url="https://climapesca.org/?json=get_tag_posts&tag_slug=actualidad";
+   $.getJSON(url,function(result){
+    $.each(result.posts, function(i, field){
+       var title=field.title;
+       var link=field.url;
+       var mid=field.id;
+       var imgpost=field.thumbnail;
+       $("#posts").append("<li> <img scr="+ imgpost+ " > <a href=\"#\" onclick= \"navigatepage('getpost.html?id="+ mid +"')\" > <h2>"+title+"</h2></a></li>");
+       //$("#posts").append("<li> <img scr="+ imgpost+ " > <a href=\"#\" onclick= \"cordova.InAppBrowser.open('" + link + "','_system'); return false; \" > <h2>"+title+"</h2></a></li>");
+    });
+   }).fail(function(jqXHR) {
+    if (jqXHR.status == 404) {
+       $("#posts").append("<li><h1>Error 404</h1></li>");
+    } else {
+        $("#posts").append("<li><h1>Error al obtener Condiciones Registradas En La Semana, compruebe su conexión.</h1></li>");
+    }
+});;
+});
+
